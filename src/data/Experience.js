e1b817b345274264e87a0d1cc08f25d7f5cec129
picuu <carloscapoforcada@gmail.com>
2025-05-19 18:34:44 +0200

    feat: update experience

--- src/data/Experience.js
+++ src/data/Experience.js
@@ -1,20 +1,27 @@
 export const EXPERIENCE = [
     {
-        date: "Actualmente...",
+        date: "Junio 2024 - Mayo 2025",
+        title: "Software Developer Intern",
+        subtitle: "Travelport",
+        description: "Desarrollé y documenté un paquete que permitía a los equipos actualizar la versión del framework de testing sin provocar un breaking change. Además realicé un artículo con una guía paso a paso de como hacer la migración. También colaboré con Interns de México para realizar un proyecto de investigación y propuesta técnica, con el objetivo de mejorar la productividad y resolver problemas técnicos diarios, que terminamos presentando los VPs de la empresa.",
+        link: "https://sites.google.com/itb.cat/dossier-practiques-carlos/"
+    }, 
+    {
+        date: "Septiembre 2023 - Mayo 2025",
         title: "CFGS en Desarrollo de Aplicaciones Web",
         subtitle: "Instituto Tecnológico de Barcelona",
-        description: "Cursé el Ciclo Formativo de Grado Superior en el ITB durante los cursos 2023 hasta 2025. Aquí cree mis primeros proyectos y adquirí conocimientos en desarrollo Frontend, bases de datos, programación Backend...",
+        description: "Formación orientada a entornos móviles. Centro de alto rendimiento.",
         link: "https://itecbcn.eu"
     }, 
     {
         date: "Marzo 2023 - Junio 2023",
-        title: "Técnico Informático",
+        title: "Técnico TIC en prácticas",
         subtitle: "Hospital Juaneda Menorca",
-        description: "Formé parte del equipo de soporte informático del Hospital Juaneda Menorca, dando soporte a los usuarios y gestionando el mantenimiento de los equipos informáticos. Además participé en el proceso migratorio del Hospital, que se mudó a un nuevo edificio.",
+        description: "Facilité el traslado del hospital a un nuevo edificio, instalando software, configurando dispositivos en red, montando ordenadores, comprobando tomas de red, asignando puertos en los racks y dando soporte informático a los trabajadores del hospital.",
         link: "https://www.juaneda.es"
     },
     {
-        date: "2021 - 2023",
+        date: "Septiembre 2021 - Junio 2023",
         title: "CFGM en Sistemas Microinformáticos y Redes",
         subtitle: "IES Biel Martí",
         description: "Realicé el Ciclo Formativo de Grado Medio en durante los cursos 2021 hasta 2023. Obtuve el título con una nota final de 10, recibiendo así matrícula de honor. Aquí aprendí los fundamentos de la informática, así como las bases de la administración de sistemas y redes.",
